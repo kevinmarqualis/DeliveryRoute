91f04976cc6c27085a37ce299558dea0e0b3c416
Kevin McBride <kevinmarqualis@gmail.com>
2022-02-22 20:30:35 -0500

    Started adding functions to Truck class

--- truck.py
+++ truck.py
@@ -1,11 +1,13 @@
 from hash_table import HashTable
+from package import Package
 
 
 class Truck:
 
     def __init__(self):
-        self.truck_storage = HashTable
+        self.truck_storage = HashTable()
 
-    def load_package(self, package):
+    def load_package(self, package: Package):
+        self.truck_storage.insert_item(package.package_id, package.address, package.deadline, package.city,
+                                       package.zipcode, package.weight, package.status)
 
-        self.truck_storage.insert_item(package)
