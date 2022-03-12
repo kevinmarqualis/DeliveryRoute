594e8cb6c2dc9be43afdcbd7c5a11ac647dee30e
Kevin McBride <kevinmarqualis@gmail.com>
2022-03-12 18:28:09 -0500

    added time tracking and updating delivered time in packages

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ city_map = Graph()
 load_distance_data(city_map)
 
 # load packages from the packages HashTable into the trucks
-truck1 = Truck()
+# start time is 8:00am
+truck1 = Truck(8, 0, 0)
 # load packages into truck 1
 truck1.load_package(packages.lookup_item('1'))
 truck1.load_package(packages.lookup_item('4'))
@@ -33,9 +34,9 @@ truck1.load_package(packages.lookup_item('34'))
 truck1.load_package(packages.lookup_item('39'))
 truck1.load_package(packages.lookup_item('40'))
 truck1.deliver_packages(city_map)
-print(truck1.miles_driven)
 
-truck2 = Truck()
+# start time is 8:00am
+truck2 = Truck(8, 0, 0)
 # load packages into truck 2
 truck2.load_package(packages.lookup_item('2'))
 truck2.load_package(packages.lookup_item('3'))
@@ -54,9 +55,11 @@ truck2.load_package(packages.lookup_item('36'))
 truck2.load_package(packages.lookup_item('37'))
 truck2.load_package(packages.lookup_item('38'))
 truck2.deliver_packages(city_map)
-print(truck2.miles_driven)
 
-truck3 = Truck()
+# start time is 10:20am waiting for updated address for package 9
+truck3 = Truck(10, 20, 0)
+package9 = packages.lookup_item('9')
+package9.address = '410 S State St'
 # load packages into truck 3
 truck3.load_package(packages.lookup_item('9'))
 truck3.load_package(packages.lookup_item('10'))
@@ -67,6 +70,5 @@ truck3.load_package(packages.lookup_item('22'))
 truck3.load_package(packages.lookup_item('23'))
 truck3.load_package(packages.lookup_item('24'))
 truck3.deliver_packages(city_map)
-print(truck3.miles_driven)
 
-print(truck1.miles_driven + truck2.miles_driven + truck3.miles_driven)
+print(round(truck1.miles_driven + truck2.miles_driven + truck3.miles_driven, 2))
--- truck.py
+++ truck.py
@@ -1,17 +1,27 @@
 from hash_table import HashTable
 from package import Package
 
+import datetime
+
 
 class Truck:
     """
     Container(HashTable) for packages
     """
+    TRUCK_MPH = 18
+    # time_obj = datetime.timedelta(hours=int(h))
 
-    def __init__(self):
+    def __init__(self, hours, minutes, seconds):
+        self.current_time = datetime.datetime(day=12, month=3, year=2022, hour=hours, minute=minutes, second=seconds)
         self.miles_driven = 0
         self.truck_storage = HashTable()
         self.delivery_locations = []
         self.location = '4001 South 700 East'
+
+    def time_to_deliver(self, miles):
+        hours = miles / self.TRUCK_MPH
+        time_taken = datetime.timedelta(hours=float(hours))
+        return time_taken
 
     def load_package(self, package: Package):
         package.status = package.delivered_statuses[1]
@@ -23,10 +33,16 @@ class Truck:
         for loc in optimized_path:
             package_id = loc[0]
             next_location = loc[1]
+
+            time_taken = self.time_to_deliver(graph.edge_distances[(self.location, next_location)])
             self.miles_driven += graph.edge_distances[(self.location, next_location)]
             self.location = next_location
+            self.current_time += time_taken
+
             current_package = self.truck_storage.lookup_item(package_id)
             current_package.status = current_package.delivered_statuses[2]
+            current_package.delivered_time = self.current_time
+        return True
 
     def find_optimized_route(self, graph):  # O(n^2)
         next_location = ''
